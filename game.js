dce28da652c9450f1fb0e87e9afd868af762dc29
KryDie <guptavedaang@gmail.com>
2026-05-03 16:50:28 -0600

    almost

--- game.js
+++ game.js
@@ -31,13 +31,78 @@ class Player {
     update(){
         this.x = lerp(this.x, this.road.laneX[this.lane], 0.12) //smoother movement
     }
+
+    draw(){
+        Image(playerimg, this.x - 30, this.y - 50, 90, 100)
+    }
+
+    changeLanes(direction = 0, reset = false){
+        if(reset){
+            this.lane = Math.floor(this.road.laneX.length / 2)
+        } else {
+            this.lane = constain(this.lane + direction, 0, this.road.laneX.length - 1) //lanes stay between 1-3
+        }
+    }
 }
 
 class Road {
+    constructor(){
+        this.laneX = [422, 595, 750]
+        this.laneY = 0
+        this.spacing = 150
+    }
+    
+    draw(){
+        fill(90,90,90); noStroke()  //road
+        rect(350,0,500,900)
+        fill('yellow')  //highlight
+        rect(350, 0, 8, 900)
+        rect(842, 0, 8, 900)
 
+
+        fill('white')
+        drawLanes()
+    }
+
+    drawLanes(){
+        let flow = 4
+
+        for(let i = 0; i < 10; i++){
+            let yPos = this.laneY + i *this.spacing
+            rect(510, yPos, 8, 80)
+            rect(690, yPos, 8, 80)
+        }
+
+        this.laneY += flow
+
+        if(this.laneY >= this.spacing){
+            this.laneY = 0
+        }
+    }
 }
 
 class Obstacle {
+    constructor(road){
+        this.road = road
+        this.lane = floor(random(3))
+        this.x = this.road.laneX[this.lane]
+        this.y = 100
+    }
 
+    update(speed){
+        this.y += speed
+    }
+
+    offScreen(){
+        return this.y > height + 50
+    }
+
+    collides(player){
+        return false
+    }
+}
+
+class CarObstac extends Obstacle {
+    
 }
 
