5563ad965a10a9592142e202254800c2c2aa165c
Vedaang Gupta <1100101470@edu.cbe.ab.ca>
2026-05-04 10:11:46 -0600

    alot of debugging needed

--- game.js
+++ game.js
@@ -1,11 +1,20 @@
 class Game {
-    constructor(){
+    constructor(assets){
+        console.log(assets);
+
+
         this.over = false
         this.started = false
         this.score = 0
         this.speed = 10
         this.spawnTimer = 0
         this.spawnEvery = 50
+        this.assets = assets
+
+        this.road = new Road()
+        this.player = new Player(this.road, this.assets.player)
+        this.obstacles = []
+
     }
 
     update(){
@@ -17,10 +26,90 @@ class Game {
             this.spawnEvery = round(random(35, 65)- this.speed)
         }
     }
+    startScreen(){
+        background('#0f122e')
+        fill('#edf10f')
+        textAlign(CENTER)
+        textSize(62)
+    
+        text('HIGHWAY DASH', width / 2, 320)
+    
+        fill('white')
+        textSize(24)
+        text('Dodge traffic and avoid potholes!', width / 2, 390)
+        text('Use ← → to switch lanes', width / 2, 430)
+    
+        fill('#2ecc71')
+        rect(width/2 - 110, 490, 220, 55, 10)
+    
+        fill('#1a1a2e')
+        textSize(26)
+        text('PRESS SPACE', width / 2, 525)
+    }
+
+    draw(){
+        background('green')
+        this.road.draw()
+        this.player.draw()
+
+        for(let obs of this.obstacles) {
+            obs.draw()
+        }
+
+        this.drawHud()
+    }
+
+    spawnObstacle(){
+        if (random() < 0.6){
+            this.obstacles.push(new CarObstac(this.road, this.assets.cars))
+        } else{
+            this.obstacles.push(new Pothole(this.road))
+        }
+    }
+
+    moveObstacle(){
+        for (let i = this.obstacles.length - 1; i >= 0; i--){
+            this.obstacles[i].update(this.speed)
+
+            if(this.obstacles[i].offScreen()){
+                this.obstacles.splice(i,1)
+            }
+        }
+    }
+
+    checkCollision(){
+        for (let obs of this.obstacles){
+            if(obs.collides(this.player)){
+                this.over = true
+            }
+        }
+    }
+
+    drawHud(){
+        fill('white'); noStroke(); textSize(22); textAlign(LEFT)
+   
+        text('SCORE: ' + floor(this.score / 10), 20, 40)
+        text('SPEED: ' + this.speed, 20, 65)
+        textAlign(RIGHT)
+        text('← → to switch lanes', width - 20, 40)
+    }
+
+    restart(){
+        this.obstacles = []
+        this.score = 0
+        this.speed = 5
+        this.over = false
+        this.spawnTimer = 0
+        this.spawnEvery = 55
+        this.player.lane = 1
+        this.player.x = road.laneX[player.lane]
+    }
 }
 
 class Player {
-    constructor(road){
+    constructor(road, img){
+        this.road = road
+        this.img = img
         this.lane = 1
         this.x = this.road.laneX[this.lane]
         this.y = 500 
@@ -33,14 +122,14 @@ class Player {
     }
 
     draw(){
-        Image(playerimg, this.x - 30, this.y - 50, 90, 100)
+        image(this.img, this.x - 30, this.y - 50, 90, 100)
     }
 
     changeLanes(direction = 0, reset = false){
         if(reset){
             this.lane = Math.floor(this.road.laneX.length / 2)
         } else {
-            this.lane = constain(this.lane + direction, 0, this.road.laneX.length - 1) //lanes stay between 1-3
+            this.lane = constrain(this.lane + direction, 0, this.road.laneX.length - 1) //lanes stay between 1-3
         }
     }
 }
@@ -61,7 +150,7 @@ class Road {
 
 
         fill('white')
-        drawLanes()
+        this.drawLanes()
     }
 
     drawLanes(){
@@ -103,6 +192,36 @@ class Obstacle {
 }
 
 class CarObstac extends Obstacle {
-    
+    constructor(road, img){
+        super(road)
+        this.img = img[floor(random(img.length))]
+    }
+
+    draw(){
+        image(this.img, this.x - 30, this.y - 50, 90, 100)
+    }
+
+    collides(player){
+        return abs(player.x - this.x) < 52 && abs(player.y - this.y) < 95
+    }
+}
+
+class Pothole extends Obstacle {
+    constructor(road){
+        super(road)
+        this.size = random(30,60)
+    }
+
+    draw(){
+        fill(58)
+        ellipse(this.x, this.y, this.size, this.size * 0.6)
+
+        fill('#362624')
+        ellipse(this.x + 3, this.y + 2, this.size * 0.5, this.size * 0.28)
+    }
+
+    collides(player){
+        return dist(player.x, player.y, this.x, this.y) < this.size * 0.45 + 22
+    }
 }
 
--- script.js
+++ script.js
@@ -1,224 +1,51 @@
-let game = {
-    over: false,
-    started: false,
-    score: 0,
-    speed: 10,
-    spawnTimer: 0,
-    spawnEvery: 50
-}
+let game 
+let assets = {}
 
-let road = {
-    laneX: [422,595,750],
-    laneY: 0,
-    spacing: 150
-}
-
-let player = {
-    lane: 1,
-    x: 600,
-    y: 500, 
-    w: 200,
-    h: 100
-}
-
-let obstacles = []
- 
-let playerImg
-let obstacleImg = []
 
 function preload(){
-    playerImg = loadImage('imgs/Black_viper.png')
-    obstacleImg[0] = loadImage('imgs/taxi.png')
-    obstacleImg[1] = loadImage('imgs/Police.png')
-    obstacleImg[2] = loadImage('imgs/Car.png')
+    assets.player = loadImage('imgs/Black_viper.png')
+    assets.cars =[ 
+        loadImage('imgs/taxi.png'),
+        loadImage('imgs/Police.png'),
+        loadImage('imgs/Car.png')
+    ] 
 }
+
 function setup(){
-    let cnv = createCanvas(1200, 900)
-    let x = (windowWidth - width) / 2
-    let y = (windowHeight - height) / 2
-    cnv.position(x, y)
+    createCanvas(1200, 900)
+    game = new Game(assets)
+    console.log(assets)
 }
 
 function draw(){
-    if (game.started == false){
-        startScreen()
+    if(game.started == false){
+        game.startScreen()
         return
-    }else if (game.over){
+    }
+
+    if(game.over){
         gameOver()
         return
     }
 
-    background("green") //grass
-    fill(90,90,90); noStroke()  //road
-    rect(350,0,500,900)
-    fill('yellow')  //highlight
-    rect(350, 0, 8, 900)
-    rect(842, 0, 8, 900)
-
-    fill('white')
-    drawLanes()
-
-    player.x = lerp(player.x, road.laneX[player.lane], 0.12) //smoother movement 
-
-    game.spawnTimer++
-    if(game.spawnTimer >= game.spawnEvery){
-        spawnObstacle()
-        game.spawnTimer = 0
-        game.spawnEvery = round(random(35, 65) - game.speed)
-    }
-
-    moveDrawObs()
-    drawCar(player.x, player.y, true,)
-    checkCollision()
-
-    game.score++
-    game.speed = min(15, 5 + floor(game.score / 500))
-
-    drawHud()
-}
-
-function startScreen(){
-    background('#0f122e')
-    fill('#edf10f'); textAlign(CENTER); textSize(62)
-    
-    text('HIGHWAY DASH', width / 2, 320)
-    fill('white'); textSize(24)
-    
-    text('Dodge traffic and avoid potholes!', width / 2, 390)
-    text('Use  ←  →  arrow keys to switch lanes', width / 2, 430)
-    fill('#2ecc71'); rect(width/2 - 110, 490, 220, 55, 10)
-    fill('#1a1a2e'); textSize(26)
-    text('PRESS SPACE', width / 2, 525)
-}
-    function gameOver(){
-    fill(0, 0, 0, 160); rect(0, 0, width, height)
-    fill('#e74c3c'); textAlign(CENTER); textSize(70)
-    text('GAME OVER', width / 2, height / 2 - 60)
-    fill('white'); textSize(30)
-    text('Score: ' + floor(game.score / 10), width / 2, height / 2 + 10)
-    fill('#edf10f'); textSize(22)
-    text('Press SPACE to play again', width / 2, height / 2 + 70)
-}
-
-function drawHud(){
-    fill('white'); noStroke(); textSize(22); textAlign(LEFT)
-    
-    text('SCORE: ' + floor(game.score / 10), 20, 40)
-    text('SPEED: ' + game.speed, 20, 65)
-    textAlign(RIGHT)
-    text('← → to switch lanes', width - 20, 40)
-}
-
-function drawLanes(){
-    let flow = 4 + floor(game.score/350)
-    for(let i = 0; i < 10; i++){
-        let yPos = road.laneY + i*road.spacing
-        rect(510, yPos, 8, 80)
-        rect(690, yPos, 8, 80)
-    }
-    road.laneY += flow
-    
-    if(road.laneY >= road.spacing){
-        road.laneY = 0
-    }
-}
-
-function drawCar(x, y, isPlayer, img){
-    if(isPlayer){
-        image(playerImg, x-30, y - 50, 90, 100)
-    } else{    
-        image(img, x-30, y-50, 90, 100)
-    }
-}
-
-function drawPothole(obs){
-    let x = obs.x
-    let y = obs.y
-    let size = obs.size
-
-    fill(58,58,58); noStroke()              //outer detail of hole
-    ellipse(x, y, size, size * 0.6)
-
-    fill('#362624')                        //mud inside pothole
-    ellipse(x + 3, y + 2, size* 0.5, size*0.28)
-}
-
-function spawnObstacle(){
-    let lane = floor(random(3))
-    let type;
-    let size = 0
-
-    if(random() < 0.6) {
-        type = 'car'
-    } else {
-        type = 'pothole'
-        size = random(30,60)
-    }
-
-    obstacles.push({
-        lane: lane,
-        x: road.laneX[lane],
-        y: -100,
-        type: type,
-        size: size,
-        img: obstacleImg[floor(random(obstacleImg.length))]
-    })
-}
-
-function moveDrawObs(){
-    for (let i = obstacles.length - 1; i >= 0; i--){
-        obstacles[i].y += game.speed
-
-        if(obstacles[i].y > 980){
-            obstacles.splice(i, 1)
-        }else if (obstacles[i].type == 'car'){
-            drawCar(obstacles[i].x, obstacles[i].y, false, obstacles[i].img)
-        }else {
-            drawPothole(obstacles[i])
-        }
-    }
-    
-}
-
-function checkCollision(){
-    for (let obs of obstacles){
-        if (obs.type == 'car'){
-            if (abs(player.x - obs.x) < 52 && abs(player.y - obs.y) < 95){
-                game.over = true
-            } 
-        } else {
-            if (dist(player.x, player.y, obs.x, obs.y) < obs.size * 0.45 + 22) {
-                game.over = true
-            }
-        }
-    }
+    game.update()
+    game.draw()
 }
 
 function keyPressed(){
-    if (!game.started && key == ' '){
+    if(game.started == false && key == ' '){
         game.started = true
         return
     }
-    if (game.over && key == ' '){
-        restart()
+
+    if(game.over && key == ' '){
+        game.restart()
         return
     }
-    if (!game.over){
-        if (keyCode == LEFT_ARROW && player.lane > 0){
-            player.lane--
-        }else if (keyCode == RIGHT_ARROW && player.lane < 2){
-            player.lane++
-        }
-    }
-}
 
-function restart(){
-    obstacles = []
-    game.score = 0
-    game.speed = 5
-    game.over = false 
-    game.spawnTimer = 0
-    game.spawnEvery = 55
-    player.lane = 1
-    player.x = road.laneX[player.lane]
+    if(keyCode == LEFT_ARROW){
+        game.player.changeLanes(-1)
+    } else if(keyCode == RIGHT_ARROW){
+        game.player.changeLanes(1)
+    }
 }
